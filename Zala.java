eefd0aa5952c08f7cec3d70f3a6f04ad2f346c4d
Samuil <samuil0240@gmail.com>
2023-06-10 12:32:47 +0300

    made the zala zadacha

---
+++ Zala.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Zala {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        double lenght = scanner.nextInt();
+        double width = scanner.nextInt();
+        double redove = lenght  / 120;
+        double coloni = (width - 100) / 70;
+
+        double mesta = (redove * coloni) - 3;
+         System.out.println(mesta);
+    }
+}
