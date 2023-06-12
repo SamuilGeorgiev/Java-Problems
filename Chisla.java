05fcb820044491cf75b0ee3621951a9249a11134
Samuil <samuil0240@gmail.com>
2023-06-12 10:59:40 +0300

    solved the second problem

---
+++ Chisla.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Chisla {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter number from 1 to 9");
+        int number = scanner.nextInt();
+
+
+        if(number >= 1 && number <= 9){
+            if(number >= 1 && number <= 3){
+                System.out.println(number * 10);
+            }else if(number >= 4 && number <= 6){
+                System.out.println(number * 100);
+            } else if (number >= 7 && number <=9) {
+                System.out.println(number * 1000);
+            }
+
+        } else if (number <= 0 || number > 9) {
+            System.out.println("Error");
+
+        }
+
+
+    }
+}
--- Zala.java
+++ Zala.java
@@ -1,7 +1,6 @@
 import java.util.Scanner;
 
-public class Zala {
-    public static void main(String[] args) {
+public class Zala {    public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
         double lenght = scanner.nextInt();
