80eba83dcbb93da7427fc39fd56f90aefb1d54f2
Samuil <samuil0240@gmail.com>
2023-07-01 10:25:47 +0300

    solved the 7th task from 9 chapter

---
+++ Zadacha11I7.java
@@ -0,0 +1,17 @@
+public class Zadacha11I7 {
+    public static void main(String[] args) {
+        int[]array = {1,3,4,5,9,7,6,5,4,3,7,9,0,5,5,6,7,5,43,9,4,};
+        int num = findFirstElement(array);
+        System.out.println("The number is:" + num);
+
+    }
+    public static int findFirstElement(int[]array){
+        for (int i = 1; i <= array.length -1; i++ ){
+            if (array[i] > array[i+1] && array[i] > array[i-1]){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+}
